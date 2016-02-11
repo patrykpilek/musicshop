0b8405f73e7699a8856962137842b9aa8f01e56a
Patryk Pilek <patryk.pilek@gmail.com>
2016-02-11 01:11:24 +0000

    template for home and admin page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var bowerDirJquery = "vendor/bower_components/jquery/dist/";
+var bowerDirBootstrap = "vendor/bower_components/bootstrap-sass/assets/";
+var bowerDirFontAwesome = "vendor/bower_components/font-awesome/";
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('musicshop.scss')
+        .copy(bowerDirBootstrap, 'resources/assets/sass/bootstrap')
+        .copy(bowerDirBootstrap + 'javascripts/bootstrap.js', 'resources/assets/js/bootstrap.js')
+        .copy(bowerDirBootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap')
+        .copy(bowerDirJquery + 'jquery.js', 'resources/assets/js/jquery.js')
+        .copy(bowerDirFontAwesome + 'scss/**', 'resources/assets/sass/fontawesome')
+        .copy(bowerDirFontAwesome + 'fonts/**', 'public/fonts/')
+    mix.scripts([
+        'js/jquery.js',
+        'js/bootstrap.js'
+    ],
+        'public/js/musicshop.js',
+        'resources/assets'
+    );
+
+    mix.sass('admin_musicshop.scss')
+        .copy(bowerDirBootstrap, 'resources/assets/sass/bootstrap')
+        .copy(bowerDirBootstrap + 'javascripts/bootstrap.js', 'resources/assets/js/bootstrap.js')
+        .copy(bowerDirBootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap')
+        .copy(bowerDirJquery + 'jquery.js', 'resources/assets/js/jquery.js')
+        .copy(bowerDirFontAwesome + 'scss/**', 'resources/assets/sass/fontawesome')
+        .copy(bowerDirFontAwesome + 'fonts/**', 'public/fonts/')
+    mix.scripts([
+            'js/jquery.js',
+            'js/bootstrap.js'
+        ],
+        'public/js/admin_musicshop.js',
+        'resources/assets'
+    );
 });
