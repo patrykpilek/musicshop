4c062f78d0954dc786630648b1a326588eaae475
Patryk Pilek <patryk.pilek@gmail.com>
2016-04-29 00:32:03 +0100

    added avatar to vabigation bar.

--- app/User.php
+++ app/User.php
@@ -15,4 +15,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'is_admin', 'remember_token',
     ];
+
+    public function getAvatar(){
+        return "https://www.gravatar.com/avatar/{{ md5($this->email) }}?d=mm&s=40";
+    }
+
+    public function getName() {
+        if($this->first_name && $this->last_name){
+            return "{$this->first_name} {$this->last_name}";
+        }
+        return null;
+    }
+
+    public function getNameOrUsername() {
+        return $this->getName() ?: $this->username;
+    }
 }
--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -7,15 +7,14 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">Music Shop</a>
+            <a class="navbar-brand" href="{!! url('/') !!}">Music Shop</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
+            <!-- Left Side of Navbar -->
             <ul class="nav navbar-nav">
-                <li class="{{ (Request::is('home') ? 'active' : '') }}"><a href="/home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li class="{{ (Request::is('home') ? 'active' : '') }}"><a href="{!! url('/home') !!}">Home</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Music<span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="true">Music<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">CD</a></li>
                         <li><a href="#">Vinyl</a></li>
@@ -25,6 +24,7 @@
                         <li><a href="#">Link Two</a></li>
                     </ul>
                 </li>
+                <li><a href="{!! url('/contact') !!}">Contact</a></li>
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
@@ -34,16 +34,14 @@
                     <li class="{{ (Request::is('login') ? 'active' : '') }}"><a href="{{ url('/login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                 @else
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            @if( Auth::user()->first_name == null)
-                                Hello, {{ Auth::user()->username }} <span class="caret"></span>
-                            @else
-                                Hello, {{ Auth::user()->first_name }} <span class="caret"></span>
-                            @endif
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-expanded="true">
+                            {{ Auth::user()->getNameOrUsername() }}
+                            <span class="caret"></span>
+                            <img class="profile-image img-circle" src="{{ Auth::user()->getAvatar() }}" >
                         </a>
                         <ul class="dropdown-menu" role="menu">
                             @if (Auth::user()->is_admin == true)
-                                <li><a href="{!! URL::to('/admin/dashboard') !!}"><i class="fa fa-tachometer"></i> Admin Dashboard</a></li>
+                                <li><a href="{!! url('/admin/dashboard') !!}"><i class="fa fa-tachometer"></i> Admin Dashboard</a></li>
                             @endif
                             <li><a href="{!! url('/user') !!}"><i class="fa fa-user"></i> Your Account</a></li>
                             <li role="separator" class="divider"></li>
