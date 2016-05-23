1e1bbe2204003d988d29a3fd1c19eb5f5ff245cc
Patryk Pilek <patryk.pilek@gmail.com>
2016-05-23 22:52:18 +0100

    fixed add to basket form.

--- resources/views/home/_album_view.blade.php
+++ resources/views/home/_album_view.blade.php
@@ -11,19 +11,18 @@
         <h2>{{ $album->album_name }}<p><small>{{ $album->artist_name }}</small></p></h2>
         <h5>{{ $album->format }}</h5>
         <p>
-        <form class="form-inline">
-            <a class="btn btn-primary" href="{{ url('/home/show/'. $album->id ) }}" role="button">
-                <i class="fa fa-eye" aria-hidden="true"></i> View details
-            </a>
-            <form method="post" role="form" action="{{ url('/basket') }}">
+            <form class="form-inline" method="post" role="form" action="{{ url('/basket') }}">
                 <input type="hidden" name="album_id" value="{{$album->id}}">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <a class="btn btn-primary" href="{{ url('/home/show/'. $album->id ) }}" role="button">
+                    <i class="fa fa-eye" aria-hidden="true"></i> View details
+                </a>
 
                 <button type="submit" class="btn btn-danger">
                     <span class="fa fa-shopping-cart"></span> Add to Basket
                 </button>
             </form>
-        </form>
         </p>
     </div>
 @endforeach
